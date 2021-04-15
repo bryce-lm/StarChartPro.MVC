93b8627b90a2cf2057395dc25590c3c38a22ba42
bryce-lm <brycetothelee@gmail.com>
2021-04-15 01:34:58 -0700

    built-out v2

---
+++ StarChartPro.MVC/Controllers/OriginController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StarChartPro.MVC.Controllers
+{
+    public class OriginController : Controller
+    {
+        // GET: Origin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
